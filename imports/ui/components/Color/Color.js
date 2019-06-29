4327312485c85de1308ebdc68de857183a844c1b
Jaired Jawed <jairedjawed@icloud.com>
2019-06-29 13:28:01 -0700

    Prevent Color Input from changing to its default on reload

--- imports/ui/components/Color/Color.js
+++ imports/ui/components/Color/Color.js
@@ -2,10 +2,6 @@ import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 import './Color.html';
 
-Template.color.onRendered(() => {
-  Session.set('color', '#000000');
-});
-
 Template.color.events({
   'change input[name="color"]': (event) => {
     Session.set('color', event.target.value);
@@ -14,6 +10,10 @@ Template.color.events({
 
 Template.color.helpers({
   color() {
+    if (!Session.get('color')) {
+      Session.set('color', '#000');
+    }
+
     return Session.get('color');
   },
 });
